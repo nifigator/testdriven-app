24fa51ec089fd7cd3d2267b91095141dc7507c00
Boris Pokhozhaev <nifigator@gmail.com>
2019-08-31 22:00:38 +0700

    Add flask_debugtoolbar

--- services/users/project/tests/test_config.py
+++ services/users/project/tests/test_config.py
@@ -39,6 +39,7 @@ class TestTestingConfig(TestCase):
             app.config['SQLALCHEMY_DATABASE_URI'] ==
             os.environ.get('DATABASE_TEST_URL')
         )
+        self.assertFalse(app.config['DEBUG_TB_ENABLED'])
 
 
 class TestProductionConfig(TestCase):
@@ -49,6 +50,7 @@ class TestProductionConfig(TestCase):
     def test_app_is_production(self):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertFalse(app.config['TESTING'])
+        self.assertFalse(app.config['DEBUG_TB_ENABLED'])
 
 
 if __name__ == '__main__':
