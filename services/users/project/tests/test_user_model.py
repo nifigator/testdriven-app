f5464c7d69e4275b05c869d0c3cce81f0e78f388
Boris Pokhozhaev <nifigator@gmail.com>
2019-09-21 14:44:59 +0700

    Add flask_bcrypt

--- services/users/project/tests/test_user_model.py
+++ services/users/project/tests/test_user_model.py
@@ -14,41 +14,42 @@ from sqlalchemy.exc import IntegrityError
 class TestUserModel(BaseTestCase):
 
     def test_add_user(self):
-        user = add_user('justatest', 'test@test.com')
-        db.session.add(user)
-        db.session.commit()
+        user = add_user('justatest', 'test@test.com', 'test')
         self.assertTrue(user.id)
         self.assertEqual(user.username, 'justatest')
         self.assertEqual(user.email, 'test@test.com')
         self.assertTrue(user.active)
+        self.assertTrue(user.password)
 
     def test_add_user_duplicate_username(self):
-        user = add_user('justatest', 'test@test.com')
-        db.session.add(user)
-        db.session.commit()
+        add_user('justatest', 'test@test.com', 'graeterthaneight')
         duplicate_user = User(
             username='justatest',
             email='test@test2.com',
+            password='greaterthaneight',
         )
         db.session.add(duplicate_user)
         self.assertRaises(IntegrityError, db.session.commit)
 
     def test_add_user_duplicate_email(self):
-        user = add_user('justauser', 'test@test.com')
-        db.session.add(user)
-        db.session.commit()
+        add_user('justauser', 'test@test.com', 'graeterthaneight')
         duplicate_user = User(
             username='justanothertest',
             email='test@test.com',
+            password='greaterthaneight',
         )
         db.session.add(duplicate_user)
         self.assertRaises(IntegrityError, db.session.commit)
 
     def test_to_json(self):
-        user = add_user('justatest', 'test@test.com')
-        db.session.add(user)
-        db.session.commit()
+        user = add_user('justatest', 'test@test.com', 'graterthaneight')
         self.assertTrue(isinstance(user.to_json(), dict))
+
+    def test_password_are_random(self):
+        user_one = add_user('jastatest', 'test@test.com', 'greaterthaneight')
+        user_two = add_user('jastatest2', 'test@test2.com',
+                            'greaterthenneight')
+        self.assertNotEqual(user_one.password, user_two.password)
 
 
 if __name__ == '__main__':
