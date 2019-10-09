66c948afdc07e627f3779509613c2cc3c44fecb2
Boris Pokhozhaev <nifigator@gmail.com>
2019-10-09 22:15:38 +0700

    Add test.sh

---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+fails=""
+
+
+inspect() {
+  if [ $1 -ne 0 ]; then
+    fails="${fails} $2"
+  fi
+}
+
+# run unit and integration tests
+docker-compose up -d --build
+docker-compose exec users python manage.py test
+inspect $? users
+docker-compose exec users flake8 project
+inspect $? users-lint
+docker-compose exec client npm test -- --coverage
+inspect $? client
+docker-compose down
+
+# return proper code
+if [ -n "${fails}" ]; then
+  echo "Test failed: ${fails}"
+  exit 1
+else
+  echo "Test passed!"
+  exit 0
+fi
